8a53ea531ac0c03cfccaeb96c25d0625e5df265d
Ansari Mohammad Ayaj Hayat Mohammad <ayajansari2002@gmail.com>
2023-12-16 10:59:53 +0530

    files added

---
+++ 1_program.js
@@ -0,0 +1,48 @@
+var x=89;
+function a(){
+     
+      function b(){
+            console.log(x);
+      }
+      return  b;
+}
+var z=a();
+z();
+
+// var x=10;
+// a();
+// b();
+// console.log(x);
+// function a(){
+//       var x=20;
+//       console.log(x);
+// }
+// function b(){
+//       var x=30;
+//       console.log(x);
+// }
+// function a(){
+      
+      
+//       c();
+//       function c(){
+//             console.log(b);
+//       }
+// }  
+// let b=10;
+// a(); 
+
+
+// {
+//       var a=10;
+//       let b=29;
+//       const c=24;
+//       console.log(a);
+//       console.log(b);
+//       console.log(c);
+
+
+// }
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
