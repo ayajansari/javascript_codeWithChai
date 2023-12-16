109ac459d6babbfed2c1f85beb22dff73d88d50f
Ansari Mohammad Ayaj Hayat Mohammad <ayajansari2002@gmail.com>
2023-12-16 16:50:04 +0000

    added new file

---
+++ basics/3_objects.js
@@ -0,0 +1,12 @@
+//
+const user={ //key-value pairs,
+    name:"ayaj",   
+    "full name":"ansari ayaj",
+    age:21,
+    email:"ayajansari@gmail.com",
+    isLoggedIn:false
+}
+//access values
+console.log(user.name)
+console.log(user["name"])   //keys are string type
+
