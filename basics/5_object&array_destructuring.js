8426f2121be7e81ea1d838bc984b4ed6815317ea
Ansari Mohammad Ayaj Hayat Mohammad <ayajansari2002@gmail.com>
2023-12-17 07:28:00 +0000

    renamed 5_object_destructuring.js

---
+++ basics/5_object&array_destructuring.js
@@ -0,0 +1,22 @@
+//object destructuring
+const obj1={
+    email:"one@gmail.com",
+    fullName:{
+        userName:{
+            firstName:"mohammad",
+            lastName:"ansari"
+        }
+    },
+    userInstructor:"james"
+}
+console.log(obj1.userInstructor)
+//below 2 lines are for above line 
+const {userInstructor:instructer}=obj1 //here we have destructed the obj1 object
+console.log(instructer)//now we can use instructer keyword for obj1's courseInstructor
+
+//array destructuring
+const arr=["name",45,"email"]
+console.log(arr[2])
+const [Name,count,mail]=arr
+console.log(count)
+console.log(Name)
